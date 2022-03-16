a88e447bea0072e5535d5619bf5b88b36cec9cd3
Ca2le <carl@jumpcode.org>
2022-03-16 14:19:44 +0100

    Added stylesheet

--- src/components/calender/calender.component.jsx
+++ src/components/calender/calender.component.jsx
@@ -8,12 +8,18 @@ class Calender extends Component {
     render(){
         const date = new Date();
         const [month, day, year] = [date.getMonth(), date.getDate(), date.getFullYear()];
-        const month_arr = ['Januari', 'Februari', 'Mars', 'April', 'Maj', 'Juni', 'Juli', 'Augusti', 'September', 'Oktober', 'November', 'December']
-        const day_arr = ['måndag', 'tisdag', 'onsdag', 'torsdag', 'fredag', 'lördag', 'söndag']
+        const month_arr = ['JAN', 'FEB', 'MAR', 'APR', 'MAJ', 'JUN', 'JUL', 'AUG', 'SEP', 'OKT', 'NOV', 'DEC']
+        const day_arr = ['mån', 'tis', 'ons', 'tor', 'fre', 'lör', 'sön']
+
         return(            
             <div className="container">
-                <div className="month_container"></div>
-                <div className="week_container"></div>
+                <div className="month_container">
+                    <h1>{month_arr[month]}</h1>
+                    <h1>{year}</h1>
+                </div>
+                <div className="week_container">
+                    {day_arr.map( days => <div><h1>{days}</h1></div>)}
+                </div>
                 <div className="day_container"></div>
             </div>
         )
