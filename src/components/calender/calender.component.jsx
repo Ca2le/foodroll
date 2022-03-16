da76e0c206f95484b6dbeff76dd14f621ff65dc8
Ca2le <carl@jumpcode.org>
2022-03-16 14:11:22 +0100

    Switched to CC from FC

--- src/components/calender/calender.component.jsx
+++ src/components/calender/calender.component.jsx
@@ -1,14 +1,24 @@
+import React, {Component} from 'react'
 
+class Calender extends Component {
+    constructor(props) {
+        super(props)
+    }
 
-function Calender() {
-    const date = new Date();
-    const [month, day, year] = [date.getMonth(), date.getDate(), date.getFullYear()];
-    const month_arr = ['Januari', 'Februari', 'Mars', 'April', 'Maj', 'Juni', 'Juli', 'Augusti', 'September', 'Oktober', 'November', 'December']
-    const day_arr = ['måndag', 'tisdag', 'onsdag', 'torsdag', 'fredag', 'lördag', 'söndag']
-    
-    return <div>
-        {console.log(month_arr[month])}
-    </div>
+    render(){
+        const date = new Date();
+        const [month, day, year] = [date.getMonth(), date.getDate(), date.getFullYear()];
+        const month_arr = ['Januari', 'Februari', 'Mars', 'April', 'Maj', 'Juni', 'Juli', 'Augusti', 'September', 'Oktober', 'November', 'December']
+        const day_arr = ['måndag', 'tisdag', 'onsdag', 'torsdag', 'fredag', 'lördag', 'söndag']
+        return(            
+            <div className="container">
+                <div className="month_container"></div>
+                <div className="week_container"></div>
+                <div className="day_container"></div>
+            </div>
+        )
+    }
+   
 }
 
 export default Calender
